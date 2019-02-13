2ab0d7839059940d256d3b89dbb6515226131776
Prrdee Geemre <18732242+prrdee@users.noreply.github.com>
2019-02-13 19:35:54 +0200

    intial commit for the portfolio app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import menu from './menu.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount(){
+    
+        var menu = document.querySelector('ul'),
+        menuLink = document.querySelector('img');
+
+        menuLink.addEventListener(
+          'click', function(e){
+            menu.classList.toggle('active');
+            e.preventDefault();
+          }
+        );
+    
+    }
+  
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+
+        <aside>
+          <figure>
+            <div id="avatar"></div>
+            <figcaption>Pradeep Ghimire</figcaption>
+          </figure>
+          <img src={menu} alt={"menu"}/>
+          <nav>
+            <ul className="active">
+              <li><a href="#">HOME</a></li>
+              <li><a href="#">SKILLS</a></li>
+              <li><a href="#">PROJECTS</a></li>
+              <li><a href="#">CONTACT</a></li>
+
+            </ul>
+          </nav> 
+        </aside>
+        <main>
+          <h1>“True knowledge exists in knowing that you know nothing.”
+          <i>- Socrates</i>
+          </h1>
+        </main>
+
+       
+
+
+      </React.Fragment>
     );
   }
 }
